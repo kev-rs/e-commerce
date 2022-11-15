24f744803aff1e1004b0e642e59ef9defd9b9d79
Kevin Brooks Santana <kevin@Kevins-MacBook-Air.local>
2022-11-15 03:18:55 -0500

    upload images to cloudinary4

--- src/pages/api/stripe.ts
+++ src/pages/api/stripe.ts
@@ -95,7 +95,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       });
       console.log('[API] - POST', req.headers);
       console.log('[API] - POST', req.headers.origin);
-      res.redirect(303, session.url!);
       res.status(200).json(session);
     } catch (err: any) {
       console.log({err});
