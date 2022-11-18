f50018e8fdcbf2240fca2eb1e9d98fca88b69979
kev <kevbs.jsx@gmail.com>
2022-11-18 13:06:07 -0500

    stripe-api

--- src/pages/api/stripe.ts
+++ src/pages/api/stripe.ts
@@ -115,22 +115,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     //   return res.status(400).json({ message: 'Order is already paid' })
     // };
     
-    const order = await prisma.order.update({
-      where: {
-        id: customer.metadata.order_id,
-      },
-      data: {
-        user: {
-          connect: {
-            id: customer.metadata.user_id
-          }
+    try {
+      const order = await prisma.order.update({
+        where: {
+          id: customer.metadata.order_id,
         },
-        paidOut: session.payment_status === 'paid',
-        transactionId: session.id,
-        total: session.amount_total! / 100
-      },
-    })
-
-    res.status(200).json({ session, customer, order });
+        data: {
+          user: {
+            connect: {
+              id: customer.metadata.user_id
+            }
+          },
+          paidOut: session.payment_status === 'paid',
+          transactionId: session.id,
+          total: session.amount_total! / 100
+        },
+      })
+  
+      res.status(200).json({ session, customer, order });
+    } catch (err) {
+      res.status(400).json({ message: err })
+    }
+    
   }
 }
