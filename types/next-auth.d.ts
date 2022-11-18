20179c93eb95736ab0ae60e52f80a40dc744a174
kev <kevbs.jsx@gmail.com>
2022-11-18 01:03:56 -0500

    types-next-auth

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,15 +1,16 @@
-import NextAuth, { DefaultSession, JWT } from "next-auth"
+import NextAuth, { DefaultSession, Session } from "next-auth"
+import { JWT } from "next-auth/jwt"
 
 declare module "next-auth" {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
   interface Session {
     user: {
       /** The user's postal address. */
       address: string
     } & DefaultSession['user']
   }
+}
+
+declare module "next-auth/jwt" {
   interface JWT {
     user: {
         name?: string | null;
@@ -19,5 +20,5 @@ declare module "next-auth" {
         status?: 'online' | 'offline', 
         // picture?: string | null;
     } | undefined
-}
+  }
 }
